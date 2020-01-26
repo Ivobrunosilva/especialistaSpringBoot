9f213b110c91d00d6961f97d433cfe59c363927f
ivobruno <ivobruno@alunos.ifce.edu.br>
2020-01-26 14:36:41 -0300

    desambiguação com anotação custimizada

--- algafood-api/src/main/java/com/example/algafood/di/notificacao/Notificador.java
+++
@@ -1,9 +0,0 @@
-package com.example.algafood.di.notificacao;
-
-import com.example.algafood.di.modelo.Cliente;
-
-public interface Notificador {
-
-	void notificar(Cliente cliente, String menssagem);
-
-}
