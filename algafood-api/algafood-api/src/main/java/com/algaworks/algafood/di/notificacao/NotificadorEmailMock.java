eb89b9324796a0d919514faa9b31589222e2a9b6
ivobruno <ivobruno@alunos.ifce.edu.br>
2020-01-26 15:01:39 -0300

    criando profiles

--- algafood-api/algafood-api/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmailMock.java
+++ algafood-api/algafood-api/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmailMock.java
@@ -4,18 +4,19 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
-@Profile("prod")
+
+@Profile("dev")
 @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 @Component
-public class NotificadorEmail implements Notificador {
+public class NotificadorEmailMock implements Notificador {
 	
-	public NotificadorEmail() {
+	public NotificadorEmailMock() {
 		System.out.println("Notificador Email Real");
 	}
 	
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
-		System.out.printf("Notificando %s através do e-mail %s: %s\n", 
+		System.out.printf("MOCK:Notificação seria enviada para %s através do e-mail %s: %s\n", 
 				cliente.getNome(), cliente.getEmail(), mensagem);
 	}
 
